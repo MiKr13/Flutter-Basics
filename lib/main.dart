2f562bc0fff6b863d80eb5b953eaa0de8755e27b
Mihir Kumar <mihirpandey.13@gmail.com>
2019-05-16 23:58:07 +0530

    Card, Rich Text and Button basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
 main() {
@@ -11,16 +12,79 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-              title: Text('Shopping List'), backgroundColor: Colors.blue),
-          body: Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assests/image_riviera_broccoli.jpg'),
-                  Text('Broccoli')
-                ],
+              title: Text('Shopping List'),
+              backgroundColor: Colors.blue[300],
+          ),
+          body: Column (
+            children: [
+              Container(
+                child: RaisedButton(
+                  onPressed: () { },
+                  elevation: 5,
+                  highlightElevation: 0,
+                  color: Colors.blue[200],
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(text: "Check "),
+                        TextSpan(
+                          text: "offers",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.lime
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  animationDuration: Duration(milliseconds: 400),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 5)
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assests/image_riviera_broccoli.jpg'),
+                    Text('Broccoli')
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                elevation: 5,
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assests/image_riviera_broccoli.jpg'),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: "Another",
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.black
+                            ),
+                          ),
+                          TextSpan(
+                            text: " oNE",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontStyle: FontStyle.italic,
+                              color: Colors.lime
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                elevation: 5,
               )
-          )
-      ),
+            ]
+          ),
+      )
     );
   }
 }
