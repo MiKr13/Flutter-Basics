33001292727ceaf4f95b009fbadb7383317367df
Mihir Kumar <mihirpandey.13@gmail.com>
2019-05-16 22:24:31 +0530

    Basic assets and card information

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Shopping List'),
-        ),
+          appBar: AppBar(
+              title: Text('Shopping List'), backgroundColor: Colors.blue),
+          body: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assests/image_riviera_broccoli.jpg'),
+                  Text('Broccoli')
+                ],
+              )
+          )
       ),
     );
   }
