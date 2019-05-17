9d0010e2a80834713f4882e9c3c03c3e57386dcd
Mihir Kumar <mihirpandey.13@gmail.com>
2019-05-17 11:41:58 +0530

    Formatting code with 4 spaces = 1 tab done

--- lib/main.dart
+++ lib/main.dart
@@ -18,72 +18,67 @@ class _AppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Shopping List'),
-        backgroundColor: Colors.blue[300],
-      ),
-      body: Column(children: [
-        Container(
-            child: RaisedButton(
-              onPressed: () {},
-              elevation: 5,
-              highlightElevation: 0,
-              color: Colors.blue[200],
-              child: RichText(
-                text: TextSpan(
-                  children: <TextSpan>[
-                    TextSpan(text: "What's "),
-                    TextSpan(
-                      text: "NEW",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontStyle: FontStyle.italic,
-                          color: Colors.lime),
-                    ),
-                  ],
-                ),
-              ),
-              animationDuration: Duration(milliseconds: 400),
-            ),
-            margin: EdgeInsets.symmetric(vertical: 5)),
-        Column(
-            children: _products
-                .map(
-                  (el) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assests/image_riviera_broccoli.jpg'),
-                            el.split(' ').length == 2
-                                ? RichText(
-                                    text: TextSpan(
-                                      children: <TextSpan>[
-                                        TextSpan(
-                                          text: el.split(' ')[0],
-                                          style: TextStyle(
-                                              decoration:
-                                                  TextDecoration.underline,
-                                              color: Colors.black),
-                                        ),
-                                        TextSpan(
-                                          text: ' ${el.split(' ')[1]}',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w300,
-                                              fontStyle: FontStyle.italic,
-                                              color: Colors.lime),
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                : Text(el),
-                          ],
-                        ),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        elevation: 5,
-                      ),
-                )
-                .toList())
+    	home: Scaffold(
+      	appBar: AppBar(
+    		title: Text('Shopping List'),
+        	backgroundColor: Colors.blue[300],
+      	),
+      	body: Column(children: [
+        	Container(
+				child: RaisedButton(
+					onPressed: () {},
+					elevation: 5,
+					highlightElevation: 0,
+					color: Colors.blue[200],
+					child: RichText(
+						text: TextSpan(
+						children: <TextSpan>[
+							TextSpan(text: "What's "),
+							TextSpan(
+							text: "NEW",
+							style: TextStyle(
+								fontWeight: FontWeight.w300,
+								fontStyle: FontStyle.italic,
+								color: Colors.lime),
+							),
+						],
+						),
+					),
+					animationDuration: Duration(milliseconds: 400),
+				),
+            	margin: EdgeInsets.symmetric(vertical: 5)
+			),
+			Column(children: _products.map((el) => Card(
+				child: Column(
+					children: <Widget>[
+					Image.asset('assests/image_riviera_broccoli.jpg'),
+					el.split(' ').length == 2
+						? RichText(
+							text: TextSpan(
+								children: <TextSpan>[
+								TextSpan(
+									text: el.split(' ')[0],
+									style: TextStyle(
+										decoration:
+											TextDecoration.underline,
+										color: Colors.black),
+								),
+								TextSpan(
+									text: ' ${el.split(' ')[1]}',
+									style: TextStyle(
+										fontWeight: FontWeight.w300,
+										fontStyle: FontStyle.italic,
+										color: Colors.lime),
+								),
+								],
+							),
+							)
+						: Text(el),
+					],
+				),
+				margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+				elevation: 5,
+			),).toList())
       ]),
     ));
   }
