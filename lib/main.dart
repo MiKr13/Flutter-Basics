2a87a7a41f1be120b83652f3292c17fcf6a033cb
Mihir Kumar <mihirpandey.13@gmail.com>
2019-05-17 11:35:37 +0530

    Stateful widget with dynamic array information

--- lib/main.dart
+++ lib/main.dart
@@ -4,85 +4,87 @@ import 'package:flutter/widgets.dart';
 
 main() => runApp(MyApp()); //for one line functions, can use =>
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AppState();
+  }
+}
+
+class _AppState extends State<MyApp> {
+  // _ makes it private to not be used by other files
+  List<String> _products = ['Broccoli', 'One more'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-              title: Text('Shopping List'),
-              backgroundColor: Colors.blue[300],
-          ),
-          body: Column (
-            children: [
-              Container(
-                child: RaisedButton(
-                  onPressed: () { },
-                  elevation: 5,
-                  highlightElevation: 0,
-                  color: Colors.blue[200],
-                  child: RichText(
-                    text: TextSpan(
-                      children: <TextSpan>[
-                        TextSpan(text: "Check "),
-                        TextSpan(
-                          text: "offers",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontStyle: FontStyle.italic,
-                            color: Colors.lime
-                          ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping List'),
+        backgroundColor: Colors.blue[300],
+      ),
+      body: Column(children: [
+        Container(
+            child: RaisedButton(
+              onPressed: () {},
+              elevation: 5,
+              highlightElevation: 0,
+              color: Colors.blue[200],
+              child: RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(text: "What's "),
+                    TextSpan(
+                      text: "NEW",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.lime),
+                    ),
+                  ],
+                ),
+              ),
+              animationDuration: Duration(milliseconds: 400),
+            ),
+            margin: EdgeInsets.symmetric(vertical: 5)),
+        Column(
+            children: _products
+                .map(
+                  (el) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assests/image_riviera_broccoli.jpg'),
+                            el.split(' ').length == 2
+                                ? RichText(
+                                    text: TextSpan(
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                          text: el.split(' ')[0],
+                                          style: TextStyle(
+                                              decoration:
+                                                  TextDecoration.underline,
+                                              color: Colors.black),
+                                        ),
+                                        TextSpan(
+                                          text: ' ${el.split(' ')[1]}',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w300,
+                                              fontStyle: FontStyle.italic,
+                                              color: Colors.lime),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                : Text(el),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                  animationDuration: Duration(milliseconds: 400),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 5)
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assests/image_riviera_broccoli.jpg'),
-                    Text('Broccoli')
-                  ],
-                ),
-                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                elevation: 5,
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assests/image_riviera_broccoli.jpg'),
-                    RichText(
-                      text: TextSpan(
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: "Another",
-                            style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              color: Colors.black
-                            ),
-                          ),
-                          TextSpan(
-                            text: " oNE",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontStyle: FontStyle.italic,
-                              color: Colors.lime
-                            ),
-                          ),
-                        ],
+                        margin:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        elevation: 5,
                       ),
-                    ),
-                  ],
-                ),
-                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                elevation: 5,
-              )
-            ]
-          ),
-      )
-    );
+                )
+                .toList())
+      ]),
+    ));
   }
 }
